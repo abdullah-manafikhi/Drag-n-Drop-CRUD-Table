4e27deed1d1eeefc9daf94896ee99fc7ec0a3dd3
abdullah-manafikhi <abdullahmanafikhi32@gmail.com>
2023-11-04 17:35:23 +0200

    troublshooting the index page layout

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function index() {
   return (
     <>
       <div className="flex justify-center my-24">
-        <h1 className='w-fit m-auto text-2xl font-bold'>This page is under construction navigate to :</h1>
-        <a className='w-fit mx-auto my-3' href="https://anc-project-1.vercel.app/output">https://anc-project-1.vercel.app/output</a>
+        <span className='w-fit text-2xl font-bold'>This page is under construction navigate to :</span>
+        <a className='mx-6 mt-1.5 underline text-blue-400' href="https://anc-project-1.vercel.app/output">https://anc-project-1.vercel.app/output</a>
       </div>
 
     </>
